a8b5163ec0c9390d10896c15fdd41abe8fae9a0a
Brandon <BrandonLuna2021@u.northwestern.edu>
2019-11-20 14:35:48 -0600

    Update Index.js

--- js/Index.js
+++ js/Index.js
@@ -2,13 +2,14 @@ var clubsDictionary = {
     "Graduate Student Club": "page2.html",
     "graduate student club": "page2.html",
     "Graduate student club": "page2.html",
-    "Red Cross Club": "page2.html",
-    "Red cross club": "page2.html",
-    "red cross club": "page2.html"
+    "Red Cross Club": "redcrossresult.html",
+    "Red cross club": "redcrossresult.html",
+    "red cross club": "redcrossresult.html"
 };
 var route = null;
 var userInputString = null;
-
+var dstList = []; //will store list of all search results hit
+var i = 0; 
 function lookupClub(){
     userInputString = document.getElementById("club_search_name").value;
     var userSubString = new RegExp(userInputString);
@@ -22,11 +23,13 @@ function lookupClub(){
         if(key == userSubString){
             route = clubsDictionary[key]; //user typed exact club name
         }else if(userSubString.test(key)){
+            dstList[i] = key; //add to list of destinations
+            i++;
             route = clubsDictionary[key]; //user tryped fraction of club name
         }
     }
 
-    if(route == null){
+    if(dstList.length == 0){
         alert("club not found :( ");
         window.location.href = "index.html";
         return;
@@ -34,6 +37,6 @@ function lookupClub(){
         window.location.href = route;
         return;
     }
-   
 }
+
 
