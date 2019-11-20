099e1b9d0446e9ca849f2e337ac2a9ceca17fbfe
Brandon <BrandonLuna2021@u.northwestern.edu>
2019-11-20 15:54:25 -0600

    added calendar event

--- js/demo.js
+++ js/demo.js
@@ -2,6 +2,7 @@ var events = [
   {'Date': new Date(2019, 11, 5), 'Title': 'Blood Drive at 3:25pm.'},
   {'Date': new Date(2020, 0, 5), 'Title': 'Red Cross Town Hall at 5pm at Tech Auditorium', 'Link': 'https://redcross.org'},
   {'Date': new Date(2020, 0, 15), 'Title': 'Red Cross Food Drive at 12pm at University Library.'},
+  {'Date': new Date(2019, 10, 5), 'Title': 'Blood Drive at 3:25pm.'}
 ];
 var settings = {};
 var element = document.getElementById('calendar');
