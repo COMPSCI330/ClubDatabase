9af80671bd805e9f31cca1980d38375e91a12526
Brandon <BrandonLuna2021@u.northwestern.edu>
2019-11-20 13:47:45 -0600

    Update Index.js

--- js/Index.js
+++ js/Index.js
@@ -1,19 +1,28 @@
 var clubsDictionary = {
     "Graduate Student Club": "page2.html",
-    "graduate student club": "page2.html"
+    "graduate student club": "page2.html",
+    "Graduate student club": "page2.html",
+    "Red Cross Club": "page2.html",
+    "Red cross club": "page2.html",
+    "red cross club": "page2.html"
 };
 var route = null;
 var userInputString = null;
 
 function lookupClub(){
     userInputString = document.getElementById("club_search_name").value;
-    if(userInputString == ""){
+    var userSubString = new RegExp(userInputString);
+    if(userSubString == ""){
         alert("error, no submission");
         return;
     }
+    
     for(var key in clubsDictionary){
-        if(key == userInputString){
-            route = clubsDictionary[key];
+        
+        if(key == userSubString){
+            route = clubsDictionary[key]; //user typed exact club name
+        }else if(userSubString.test(key)){
+            route = clubsDictionary[key]; //user tryped fraction of club name
         }
     }
 
@@ -27,3 +36,4 @@ function lookupClub(){
     }
    
 }
+
